c8095cb6a796ed82cf255daa6488838b11df214b
E-zn131 <eyadzanaty131@gmail.com>
2024-12-17 23:43:20 +0200

    Static

--- job/urls.py
+++ job/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Job_type),
-    path('<int: id>', views.Job_details),
+    path('<int:id>', views.Job_details),
 ]
--- job/views.py
+++ job/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 def Job_type(request):
-    pass
+    jobs = Job.objects.all()
+    context = {'jobs' : jobs}
+    return render(request, 'job/job_type.html' , context)
 
 def Job_details(request, id):
-    pass
+    job = Job.objects.get(id= id)
+    context = {'job': job}
+    return render(request, 'job/job_details.html', context)
